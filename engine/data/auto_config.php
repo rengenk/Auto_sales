fe72e22dd160d1349d17b6b7a980f431544094bc
rengenk <36222777+rengenk@users.noreply.github.com>
2018-12-10 12:35:31 +0300

    Create auto_config.php

---
+++ engine/data/auto_config.php
@@ -0,0 +1,47 @@
+<?PHP
+
+$autoConfig['nummodels']                = 0;
+
+$autoConfig['num_price']                = 40;
+
+$autoConfig['allowed_addauto']        = 1;
+
+$autoConfig['allowed_autodelete']        = 1;
+
+$autoConfig['allowed_screen']      	 = 'jpg,jpeg,gif,png';
+
+$autoConfig['widththumb']      	 = 100;
+
+$autoConfig['width_photo']      	 = 640;
+
+$autoConfig['jpeg_quality']            = 60;
+
+$autoConfig['maxsize_thumb']            = 1024000;
+
+$autoConfig['allow_watermark']     	  = 1;
+
+$autoConfig['allow_watermark_for_thumbs']  = 0;
+
+$autoConfig['max_watermark']       	= 0;
+
+$autoConfig['allow_screenshot']     	  = 1;
+
+$autoConfig['allow_edit_price']     	  = 1;
+
+$autoConfig['vip_group']     	  = 1;
+
+$autoConfig['allow_sell_price']     	  = 1;
+
+$autoConfig['allow_vip_block']     	  = 1;
+
+$autoConfig['allow_last_block']     	  = 1;
+
+$autoConfig['num_last_block']     	  = 5;
+
+$autoConfig['num_days']     	  = 15;
+
+$autoConfig['start_year']     	  = 1983;
+
+$autoConfig['search_form']     	  = 1;
+
+?>
